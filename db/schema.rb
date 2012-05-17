2877cf761efda59a56e91080a06d1b398488448a
Suleiman Tawil <stawil@webafterweb.com>
2012-05-17 20:52:20 +0100

    Original Verision with one Table

--- db/schema.rb
+++ db/schema.rb
@@ -11,18 +11,11 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120516201516) do
+ActiveRecord::Schema.define(:version => 20120516195510) do
 
   create_table "products", :force => true do |t|
     t.string   "name"
     t.string   "image"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
-
-  create_table "supermarket", :force => true do |t|
-    t.string   "name"
-    t.string   "logo"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
